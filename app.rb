8c7abf30038a1449e15629a3eee9258b7a663d57
Amit <amit.evron@gmail.com>
2016-06-19 12:12:08 +0000

    completed assignment. All cucumber tests pass

--- app.rb
+++ app.rb
@@ -39,6 +39,8 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
+    #result = @game.guess(letter)
+    flash[:message]="You have already used that letter" unless @game.guess(letter)
     ### YOUR CODE HERE ###
     redirect '/show'
   end
@@ -49,7 +51,12 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    ### YOUR CODE HERE ### 
+    if @game.check_win_or_lose == :win
+	redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+	redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
